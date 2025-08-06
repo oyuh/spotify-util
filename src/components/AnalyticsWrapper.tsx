9e45f5d3fd8fb0b772696761d1ca642f536c6f4f
oyuh <lawsonhart0919@gmail.com>
2025-08-06 01:50:18 -0500

    FOR THE LOVE OF FUCKING CHRIST

--- src/components/AnalyticsWrapper.tsx
+++ src/components/AnalyticsWrapper.tsx
@@ -2,12 +2,16 @@
 
 import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 export function AnalyticsWrapper() {
   return (
     <>
       <Analytics />
       <SpeedInsights />
+      {injectSpeedInsights()}
+      {inject()}
     </>
   )
 }
